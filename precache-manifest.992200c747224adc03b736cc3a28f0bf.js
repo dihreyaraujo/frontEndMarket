e78c7615b64444b921572065b8fc6b440462ecbd
dihreyaraujo <dihreyaraujo@users.noreply.github.com>
2022-10-12 19:36:02 +0000

    deploy: 17c17a35aebbb961957520789158ef25eff3ffdc

---
+++ precache-manifest.992200c747224adc03b736cc3a28f0bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e2731ba932a08723080936851f2e374",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d61c8fd32d403d0a1ecd",
+    "url": "/static/css/main.ecd219bd.chunk.css"
+  },
+  {
+    "revision": "ff04754688a426d63912",
+    "url": "/static/js/2.05bef05a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.05bef05a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d61c8fd32d403d0a1ecd",
+    "url": "/static/js/main.1bf3e239.chunk.js"
+  },
+  {
+    "revision": "b07afeb638d6c35be808",
+    "url": "/static/js/runtime-main.5de205f4.js"
+  }
+]);
