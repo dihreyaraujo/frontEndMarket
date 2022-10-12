3d768d837bfb5565a62c5cc67e6b195fd7aa9b60
Dihrey Araujo <99283526+dihreyaraujo@users.noreply.github.com>
2022-10-12 16:31:18 -0300

    arrumando rel

--- src/Component/Checkout.js
+++ src/Component/Checkout.js
@@ -9,7 +9,7 @@ class Checkout extends React.Component {
         <div className="ty">
           <h1>Isso foi apenas uma simulação do meu projeto de e-commerce</h1>
           <p>Obrigado por testá-lo ! ! !</p>
-          <a href="https://dihrey.netlify.app/" rel="noreferrer" target="_blank">Meu Portfólio</a>
+          <a href="https://dihrey.netlify.app/" target="_blank">Meu Portfólio</a>
         </div>
         <div className="div-seta-check">
           <img src="https://cdn-icons-png.flaticon.com/512/20/20901.png?w=360" alt="seta" className="seta" />
